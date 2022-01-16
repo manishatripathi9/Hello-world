9dce62fd90c7a85146d42eee7b10631320e07af4
Nitish Upadhyay <manishatripathi9@gmail.com>
2022-01-16 11:05:29 +0530

    First commit

---
+++ src/test/Javademo.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author nitis
+ *
+ */
+public class Javademo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+
+	}
+
+}
